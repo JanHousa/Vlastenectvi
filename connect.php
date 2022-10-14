c87d3d0afd0a221be0d6c2994fe9e8df5cc5b2c0
Raccoon <honzohousa@gmail.com>
2022-10-14 20:32:49 +0200

    commit

--- connect.php
+++ connect.php
@@ -1,4 +1,18 @@
 <?php
-    $
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
+    $conn = new mysqli("localhost","root","","register");
+    if($conn->connect_error){
+        die("Connection Failed : ".$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into registration(email, password)
+            values(?, ?)");
+        $stmt->bind_param("ss",$email, $password);
+        $stmt->execute();
+        echo "registrace proběhla úspěšně...";
+        $stmt->close();
+        $conn->close();
+
+    }
 ?>
